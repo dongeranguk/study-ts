b3e92ebf7ab153d6e330b1395c98992b6955a220
dongeranguk <rooetym@naver.com>
2024-09-22 01:24:32 +0900

    컴파일러 옵션 지정 시 실습한 파일들이 커밋 과정에서 누락되어 이를 추가하였다. 	new file:   section1/dist/hello.js 	new file:   section1/dist/index.js 	new file:   section1/src/a.ts 	new file:   section1/src/b.ts 	new file:   section1/src/hello.ts 	modified:   section1/src/index.ts

--- section1/dist/index.js
+++ section1/dist/index.js
@@ -1,12 +1,9 @@
 console.log('Hello TypeScript');
-const a : number = 1;
+const a = 1;
 // const b : number = "hello";
 const b = 2;
 // b.toUpperCase();
-
 console.log(a + b);
-
 const func = () => console.log("Hello");
-
 import { hello } from "./hello";
-console.log(hello);
+console.log('This is ' + hello);
